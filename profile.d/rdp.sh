176df3775cecf65cebccac1e5e78d87f92f2d0d0
Rafal Sladek <rafal.sladek@gmail.com>
2016-04-04 13:06:13 +0200

    rdp command with full qualified dns name

--- profile.d/rdp.sh
+++ profile.d/rdp.sh
@@ -7,6 +7,6 @@ rdp(){
 	fi
 	echo "RDP geometry: $geometry"
 	host=$1
-	(rdesktop -g $geometry -k de -PNDz -a 16 -x l -r clipboard:PRIMARYCLIPBOARD -r sound:off -r disk:archie=/home/rsladek/win_share -d AS24 $host:3389)& 
+	(rdesktop -g $geometry -k de -PNDz -a 16 -x l -r clipboard:PRIMARYCLIPBOARD -r sound:off -r disk:archie=/home/rsladek/win_share -d AS24 $host.as24.local:3389)& 
 }
 export rdp
