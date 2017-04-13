75981a219e275ae61d0a5e91615e212d8d6342a6
Rafal Sladek <rafal.sladek@gmail.com>
2017-04-13 11:54:54 +0200

    added as24repos function to list all repos

---
+++ fish/bin/as24repos.fish
@@ -0,0 +1,8 @@
+set -x repoUser autoscout24
+
+curl -q \
+	-u "$as24_git_token:x-oauth-basic" \
+	-sk "https://api.github.com/orgs/$repoUser/repos?type=private&sort=created&direction=asc&per_page=200" \
+| grep '"name"' \
+| awk -F': "' '{print $2}' \
+| sed -e 's/",//g'
