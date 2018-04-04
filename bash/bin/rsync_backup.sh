8f02af337a8783dd1d5f4590eb430a8964dbe5e2
Rafal <rafal.sladek@gmail.com>
2018-04-04 23:11:49 +0200

    added rsync backup script

--- bash/bin/rsync_backup.sh
+++ bash/bin/rsync_backup.sh
@@ -3,4 +3,9 @@ RSYNC_SERVER_DIR=mac
 RSYNC_SERVER=192.168.3.3
 RSYNC_USER=rsync
 
-rsync -avP --progress ~/Library/Application\ Support/Chaincoin  rsync://${RSYNC_USER}@${RSYNC_SERVER}:/${RSYNC_SERVER_DIR}
+cat rsync_sources.txt | while read line
+do
+    echo ${line}
+    rsync -avP --progress "${line}"  rsync://${RSYNC_USER}@${RSYNC_SERVER}:/${RSYNC_SERVER_DIR}   
+done
+
