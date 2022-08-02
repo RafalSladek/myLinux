a4b06af16609880ed0ca4465b9104ce9d2b1aa93
Rafal Sladek <rafal.sladek@gmail.com>
2022-08-02 11:00:07 +0200

    add new aliases

--- zsh/.oh-my-zsh/custom/02_aliases.zsh
+++ zsh/.oh-my-zsh/custom/02_aliases.zsh
@@ -1,3 +1,4 @@
+export PATH=$PATH:/Users/rsladek/Library/Python/3.9/bin/
 alias hostswitcher="sudo ruby ~/sources/rafalsladek/hostswitcher/hostswitcher.rb"
 alias md5sum=md5
 
@@ -8,3 +9,13 @@ alias as24='cd ~/sources/autoscout24'
 
 alias untar='tar -zxvf'
 alias untarxz='tar -xJf'
+alias bastion='aws cloudformation list-exports --query \"Exports[?Name=='\''ConnectCommandBastionHost'\''].Value\" --no-paginate --output text'
+alias sfa="scloud account login as24-finance AdminAccess"
+alias sfp="scloud account login as24-finance PowerUserAccess"
+
+alias soda="scloud account login as24-cashstack-offerb2c-dev AdminAccess"
+alias sopa="scloud account login as24-cashstack-offerb2c-prod AdminAccess"
+alias sdsa="scloud account login as24-direct-sale AdminAccess"
+alias spea="scloud account login as24-partner-experience AdminAccess"
+alias sma="scloud account login as24-marketplace AdminAccess"
+alias spsa="scloud account login as24-private-seller AdminAccess"
